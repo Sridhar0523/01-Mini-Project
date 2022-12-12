db5e42cfdc48d0affb5cdc70275e50aaca94a727
Sridhar0523 <log4sridhar@gmail.com>
2022-12-12 07:48:43 +0530

    Updated ContactRestController

--- src/main/java/in/phonebookapplication/controller/ContactRestController.java
+++ src/main/java/in/phonebookapplication/controller/ContactRestController.java
@@ -3,45 +3,65 @@ package in.phonebookapplication.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.phonebookapplication.model.Contact;
+import in.phonebookapplication.service.ContactService;
 
-
+@RestController
+@RequestMapping("/contacts")
 public class ContactRestController {
+
+	@Autowired
+	private ContactService service;
 
 	// Create Contact
 	@PostMapping("/save")
-	public Contact saveContact() {
-		return null;
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact) {
+		String saveContact = service.saveContact(contact);
+		return new ResponseEntity<String>("Contact '" + saveContact + "' saved", HttpStatus.OK);
 	}
 
 	// Display All Contacts
 	@GetMapping("/all")
-	public List<Contact> getAllContacts() {
-		return null;
+	public ResponseEntity<List<Contact>> getAllContacts() {
+		List<Contact> list = service.getAllContacts();
+		return new ResponseEntity<List<Contact>>(list, HttpStatus.OK);
 	}
-	
-	//get ContactById
+
+	// get ContactById
 	@GetMapping("/contact/{id}")
-	public ResponseEntity<Map<String, Boolean>> getContactById() {
-		return null;
+	public ResponseEntity<Contact> getContactById(@PathVariable Integer id) {
+		Contact contact = service.getContactById(id);
+		return new ResponseEntity<Contact>(contact, HttpStatus.OK);
 	}
 
 	// Edit and Update Contact Details
 	@PutMapping("/modify/{id}")
-	public ResponseEntity<Contact> updateContact() {
-		return null;
+	public ResponseEntity<String> updateContact(@PathVariable Integer id, @RequestBody Contact contact) {
+		Contact contactById = service.getContactById(id);
+		contactById.setContactName(contact.getContactName());
+		contactById.setContactEmail(contact.getContactEmail());
+		contactById.setContactNumber(contact.getContactNumber());
+		service.saveContact(contactById);
+		return new ResponseEntity<String>("Contact '" + id + "' Updated", HttpStatus.OK);
 	}
-	
-	//Delete Contact
+
+	// Delete Contact
 	@DeleteMapping("/contact/{id}")
-	public ResponseEntity<Map<String, Boolean>> deleteContactById() {
-		return null;
+	public ResponseEntity<String> deleteContactById(@PathVariable Integer id) {
+		service.deleteContactById(id);
+		return new ResponseEntity<String>("Contact '" + id + "' Deleted", HttpStatus.OK);
 	}
 
 }
--- src/main/java/in/phonebookapplication/service/impl/ContactServiceImpl.java
+++ src/main/java/in/phonebookapplication/service/impl/ContactServiceImpl.java
@@ -1,46 +1,72 @@
 package in.phonebookapplication.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import in.phonebookapplication.model.Contact;
 import in.phonebookapplication.repository.ContactRepository;
 import in.phonebookapplication.service.ContactService;
 
+@Service
 public class ContactServiceImpl implements ContactService {
-	
+
 	@Autowired
 	private ContactRepository repo;
 
 	@Override
 	public String saveContact(Contact contact) {
 
-		return null;
+		// insertion
+		contact = repo.save(contact);
+
+		if (contact.getContactId() != null) {
+			return "Contact Saved";
+		} else {
+			return "Contact failed to save";
+		}
 	}
 
 	@Override
 	public List<Contact> getAllContacts() {
 
-		return null;
+		return repo.findAll();
 	}
 
 	@Override
 	public Contact getContactById(Integer contactId) {
 
+		Optional<Contact> findById = repo.findById(contactId);
+
+		if (findById.isPresent()) {
+			return findById.get();
+		}
 		return null;
 	}
 
+	// Update Operation
 	@Override
 	public String updateContact(Contact contact) {
 
-		return null;
+		if (repo.existsById(contact.getContactId())) {
+			repo.save(contact);
+			return "Update Success";
+		} else {
+			return "No Record Found";
+		}
 	}
 
 	@Override
 	public String deleteContactById(Integer contactId) {
 
-		return null;
+		if (repo.existsById(contactId)) {
+			repo.deleteById(contactId);
+			return "Record Deleted";
+		} else {
+			return "No Record Found";
+		}
 	}
 
 }
